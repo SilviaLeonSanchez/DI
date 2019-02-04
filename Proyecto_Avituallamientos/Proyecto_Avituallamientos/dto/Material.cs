82a4786ef0a4e6262c058b05d200cadbe9d95c5f
Silvia Leon Sanchez <silviaoposita@gmail.com>
2019-02-04 09:56:16 +0100

    Solucinado refactor pantallas

---
+++ Proyecto_Avituallamientos/Proyecto_Avituallamientos/dto/Material.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Avituallamientos.Modelos
+{
+    public class Material
+    {
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public TIPO_MATERIAL Tipo { get; set; }
+        public float Precio { get; set; }
+
+        public override string ToString()
+        {
+            return "Nombre: "+Nombre+"\tTipo material: "+Tipo+"\tPrecio: "+Precio;
+        }
+
+    }
+
+    public enum TIPO_MATERIAL
+    {
+        BEBIDA, COMIDA, MATERIAL_SANITARIO
+    }
+}
---
+++ Proyecto_Avituallamientos/Proyecto_Avituallamientos/logica/LogicaNegocio.cs
@@ -0,0 +1,35 @@
+﻿using Proyecto_Avituallamientos.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Avituallamientos.logica
+{
+    class LogicaNegocio
+    {
+
+        public ObservableCollection<Carrera> Carreras { get; set; }
+        public ObservableCollection<Material> Materiales {get; set;}
+        public static LogicaNegocio INSTANCE;
+
+        public static LogicaNegocio getInstance()
+        {
+            if (INSTANCE == null)
+            {
+                INSTANCE = new LogicaNegocio();
+            }
+            return INSTANCE;
+        }
+
+        private LogicaNegocio()
+        {
+            Materiales = new ObservableCollection<Material>();
+            Carreras = new ObservableCollection<Carrera>();
+        }
+
+
+    }
+}
