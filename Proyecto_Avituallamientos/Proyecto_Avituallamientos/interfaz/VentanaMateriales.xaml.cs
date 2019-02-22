3927b8a5a5a7cfe0fb59b3b4c37142494d860255
Silvia Leon Sanchez <silviaoposita@gmail.com>
2019-02-22 14:27:32 +0100

    ...

--- Proyecto_Avituallamientos/Proyecto_Avituallamientos/interfaz/VentanaMateriales.xaml.cs
+++ Proyecto_Avituallamientos/Proyecto_Avituallamientos/interfaz/VentanaMateriales.xaml.cs
@@ -46,19 +46,51 @@ namespace Proyecto_Avituallamientos.interfaz
 
         private void ButtonAniadirMaterial_Click(object sender, RoutedEventArgs e)
         {
-            Material material = (Material) DataGridMateriales.SelectedItem;
-            if (material != null)
+            int cantidad = 0;
+            if (string.IsNullOrEmpty(this.TextBoxCantidad.Text))
             {
-                int cantidad = Convert.ToInt32(this.TextBoxCantidad.Text);
-                if (this.avituallamiento.Materiales.ContainsKey(material))
-                {
-                    this.avituallamiento.Materiales[material] += cantidad; 
-                }
-                else
-                {
-                    this.avituallamiento.Materiales.Add(material, cantidad);
-                }
+                MessageBox.Show("Introduce una cantidad");
             }
+            else
+            {
+                try
+                {
+                    cantidad = Convert.ToInt32(this.TextBoxCantidad.Text);
+                    if (cantidad <= 0)
+                    {
+                        MessageBox.Show("La cantidad debe ser un numero entero positivo");
+                    }
+                    else
+                    {
+                        Material material = (Material)DataGridMateriales.SelectedItem;
+                        if (material != null)
+                        {
+                            if (this.avituallamiento.Materiales.ContainsKey(material))
+                            {
+                                this.avituallamiento.Materiales[material] += cantidad;
+                            }
+                            else
+                            {
+                                this.avituallamiento.Materiales.Add(material, cantidad);
+                            }
+                            MessageBox.Show("Material añadido correctamente");
+                        }
+                    }
+                }
+                catch (FormatException ex)
+                {
+                    MessageBox.Show("La cantidad debe ser un numero entero positivo");
+                }
+
+            }
+
+
+
+
+
+            
+               
+            
         }
 
         private void ButtonBorrarMaterial_Click(object sender, RoutedEventArgs e)
